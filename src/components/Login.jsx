0afff0c003196fe33efd8be639e9863b0004e3d5
Md. Jahirul Islam <jahirulislam12283@gmail.com>
2023-04-26 17:28:39 +0600

    added login and registation components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+    }
     return (
-        <div>
-            <h2>Login</h2>
-        </div>
+        <>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold"> Please Login !</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleLogin} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' required placeholder="email" className="input input-bordered" />
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' required placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                        <Link to='/register' className="btn btn-link">
+                            <button className='btn btn-link'>New to AuthMaster?</button>
+                        </Link>
+
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
