7b6e9d2337762e43d76c48337a3e35356485e42e
Md. Jahirul Islam <jahirulislam12283@gmail.com>
2023-04-26 13:06:03 +0600

    install tailwind , daisy ui , firebase setup and install added navbar

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-nk9mg32fRXpP-SUnWBSVCR9eWNQbbd0",
+  authDomain: "auth-firebase-context-ta-b474c.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-b474c",
+  storageBucket: "auth-firebase-context-ta-b474c.appspot.com",
+  messagingSenderId: "10959786261",
+  appId: "1:10959786261:web:5b6849dfc98392e330d91c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
